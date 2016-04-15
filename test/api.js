644af0ec3eb3122c6248795350c6361d5b7ab5c9
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-04-15 13:44:38 -0400

    test for valid path

--- test/api.js
+++ test/api.js
@@ -7,22 +7,25 @@ import test from 'ava';
 import uniqueTempDir from 'unique-temp-dir';
 import shotPut from '../';
 
-test.cb('.watch() rejects desktop dest path', t => {
-  t.plan(2);
-  const dest = '/desktop';
-  child_process.execFile('../cli.js', ['.js', dest], (err, stdout, stderr) => {
-    t.ifError(err);
-    t.is(stderr, 'dir must be a directory other than /desktop\n');
-    t.end();
-  })
-})
+const ext = '.js';
 
 test.cb('.watch() rejects non-existing dest path', t => {
   t.plan(2);
   const dest = uniqueTempDir();
-  child_process.execFile('../cli.js', ['.js', dest], (err, stdout, stderr) => {
+  child_process.execFile('../cli.js', [ext, dest], (err, stdout, stderr) => {
     t.ifError(err);
     t.is(stderr, `${dest} is not a valid directory\n`);
+    t.end();
+  })
+})
+
+test.cb('.watch() listens when given valid dest path', t => {
+  t.plan(1);
+  const dest = '/documents';
+  const sp = child_process.spawn('../cli.js', [ext, dest]);
+
+  sp.stdout.on('data', (data) => {
+    t.is(data.toString(), `watching ${path.sep}desktop for new ${ext} files..\n`)
     t.end();
   })
 })
