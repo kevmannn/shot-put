e9f97f62d17ab4a864be07db39750f0ed6ddd2a3
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-06-09 21:00:22 -0400

    begin implementation of rename

--- cli.js
+++ cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 'use strict';
-
 const path = require('path');
 const meow = require('meow');
 const chalkForm = require('chalk-form');
@@ -9,7 +8,7 @@ const shotPut = require('./');
 
 const cli = meow(`
   Usage:
-    $ shot-put <ext dir> Watch desktop for 'ext' files and move them to 'dir'
+    $ shot-put <ext dir> Watch /desktop for 'ext' files and move them to 'dir'
 
   Options:
     --preserve="<filenames>" Prevent specific files from ever being moved
@@ -24,26 +23,24 @@ const cli = meow(`
   }
 })
 
-const input = cli.input;
-const flags = cli.flags;
-
 let sourceStr = '';
-const destStr = chalkForm(['green', 'bold'])(input[1]);
+const destStr = chalkForm(['green', 'bold'])(cli.input[1]);
 
 const write = str => process.stdout.write(str);
 
 shotPut.ps.on('watch', src => {
   sourceStr = chalkForm(['dim'])(src);
-  write(`watching ${sourceStr} for new ${chalkForm(['bold', 'cyan'])(input[0])} files..\n`);
+  write(`watching ${sourceStr} for new ${chalkForm(['bold', 'cyan'])(cli.input[0])} files..\n`);
 })
 
 shotPut.ps.on('moved', file => {
   log(`..moved ${chalkForm(['italic', 'dim'])(file)}\n`);
+  // promptRename(file);
 })
 
-shotPut.watch(input[0], input[1], flags)
+shotPut.watch(cli.input[0], cli.input[1], cli.flags)
   .then(info => {
-    const numMovedStr = chalkForm(['cyan', 'bold'])(`${info.moved.length} ${input[0]}`);
+    const numMovedStr = chalkForm(['cyan', 'bold'])(`${info.moved.length} ${cli.input[0]}`);
 
     if (process.env.FORK) {
       process.send({ movedFiles: info.moved, preservedFiles: info.preserved });
@@ -55,5 +52,24 @@ shotPut.watch(input[0], input[1], flags)
     process.exit(0);
   })
   .catch(err => {
-    process.stderr.write(chalkForm(['red', 'bold'])(err));
+    process.stderr.write(err);
   })
+
+function promptRename(file) {
+  write(`> rename ${file} (y/n) ?  `);
+
+  process.stdin.setRawMode(true);
+  process.stdin.on('readable', initRename.bind(null, file, process.stdin.read()));
+}
+
+function initRename(filename, ynInput) {
+  const negation = new Set(['\u0003']);
+  const resolution = new Set(['\r', '\t', 121, 89]);
+
+  if (ynInput === null || negation.indexOf(ynInput) !== -1) return false;
+
+  write('>\n');
+  process.stdin.on('readable', () => {
+    shotPut.rename(process.stdin.read(), err => {})
+  })
+}
